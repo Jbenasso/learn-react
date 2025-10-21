40e6afc3fe78b087a4bd5cf370263944ffbacb5c
Jbenasso <benassoj@email.sc.edu>
2025-10-21 12:30:58 -0400

    feat: implement routing with react-router-dom and create Home and About pages

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import './css/index.css';
+import Layout from './Layout.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+
+export default function App() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hi</h1>
-    <h2>Loving this</h2>
+   <App />
   </React.StrictMode>
 );
 
